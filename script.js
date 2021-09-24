28a9004ec07ad5567e36cb43bdd17f45a84daaff
volTzWRLD <82188352+volTzWRLD@users.noreply.github.com>
2021-09-24 15:00:32 -0300

    TO-DO TASKS
    For studies

---
+++ script.js
@@ -0,0 +1,139 @@
+
+
+const ul_element    = document.querySelector('ul');
+const input_element = document.getElementById("input");
+const buttonAdd     = document.getElementById("add");
+const buttonRem     = document.getElementById("remall");
+
+
+
+
+
+
+const tasks = JSON.parse(localStorage.getItem("key")) || []
+
+    
+
+function showTasks(){
+
+    ul_element.innerHTML = ''
+
+    for (list of tasks){
+
+        const element_of_li   = document.createElement('li');
+        const text_of_element = document.createTextNode(list);
+        const element_img     = document.createElement('img');
+
+        
+        element_of_li.appendChild(text_of_element);
+        element_of_li.appendChild(element_img);
+        ul_element.appendChild   (element_of_li);
+
+
+        element_img.src   = './images/user-trash-icon.png';
+        element_img.align = 'right';
+
+        element_img.setAttribute("onclick", `remTasks(this)`);
+
+        
+        
+    }
+
+
+    
+
+}
+
+
+showTasks()
+
+
+function addTasks(){
+
+    const verify_input  = input_element.value == '' || input_element.value == ' ' ? alert("Preencha o campo, por favor") : true; 
+
+    if (verify_input){
+
+        const element_in_li = input_element.value;
+        tasks.push(element_in_li);
+    
+
+        input_element.value = '';
+        showTasks();
+        Storage()
+        
+    }
+
+
+
+
+
+}
+
+buttonAdd.setAttribute("onclick", 'addTasks()')
+
+
+function remTasks(e){
+
+    tasks.splice(e.target, 1)
+    showTasks()
+    Storage()
+
+ 
+
+
+}
+
+
+buttonRem.setAttribute("onclick", 'remAllTasks()')
+
+function remAllTasks(){
+    
+    localStorage.removeItem("key");
+    ul_element.innerHTML = '';
+    
+}
+
+function Storage(){
+
+    localStorage.setItem("key", JSON.stringify(tasks))
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
